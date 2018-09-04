0557cb6238568025de981de576483887282119d2
Prasanna-Loga <36530081+Prasanna-Loga@users.noreply.github.com>
2018-09-04 13:06:14 +0530

    Add files via upload
    collections method with lambda expression

--- callableCollections/CollectionCallableDemo.java
+++ callableCollections/CollectionCallableDemo.java
@@ -17,34 +17,23 @@ public class CollectionCallableDemo {
 			ExecutorService exec = Executors.newFixedThreadPool(5);
 			Set<Callable<String>> callableCollections = new HashSet<Callable<String>>() ; 
 			System.out.println("Current Thread executing  in main : " + Thread.currentThread().getName());
-			callableCollections.add(new Callable<String>(){
-				@Override
-				public String call() throws Exception {
-					// TODO Auto-generated method stub
+			callableCollections.add(() -> {
 					Thread.sleep(1000);
-					System.out.println("Current Thread executing : " + Thread.currentThread().getName());
+					System.out.println("Current Thread executing in call 1: " + Thread.currentThread().getName());
 					return "Task 1";
-				}
-			}); 
+				}); 
 			
-			callableCollections.add(new Callable<String>(){
-				@Override
-				public String call() throws Exception {
-					// TODO Auto-generated method stub
+			callableCollections.add(() -> {
 					Thread.sleep(10);
-					System.out.println("Current Thread executing : " + Thread.currentThread().getName());
+					System.out.println("Current Thread executing in call 2: " + Thread.currentThread().getName());
 					return "Task 2";
-				}
-			});
+				});
 			
-			callableCollections.add(new Callable<String>(){
-				@Override
-				public String call() throws Exception {
+			callableCollections.add(() -> {
 					// TODO Auto-generated method stub
 					Thread.sleep(100);
-					System.out.println("Current Thread executing : " + Thread.currentThread().getName());
+					System.out.println("Current Thread executing in call 3: " + Thread.currentThread().getName());
 					return "Task 3";
-				}
 			});
 			
 			String result = exec.invokeAny(callableCollections);
